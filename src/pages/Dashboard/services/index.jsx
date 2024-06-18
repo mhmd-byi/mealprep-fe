34cbff22394a071d5ea1a3f608852bf5e57169ff
abbasbadshah <abbasbadshah603@gmail.com>
2024-06-18 16:41:41 +0530

    removing grid and using flex method in desktop screen

--- src/pages/Dashboard/services/index.jsx
+++ src/pages/Dashboard/services/index.jsx
@@ -20,130 +20,130 @@ export const Services = () => {
           <p className="text-4xl sm:text-2xl">Our Services</p>
         </div>
       </section>
-      <section
-        className={`mb-20 mx-auto grid ${
-          isSmallScreen ? "grid-cols-2 gap-4" : "grid-cols-4 gap-10"
-        } max-w-[1500px]`}
-      >
-        <div
-          className="flex flex-col w-full h-[150px] sm:w-[200px] sm:h-[200px] p-2 sm:p-5 gap-2 sm:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
-          onClick={() => navigation()}
-        >
-          <RestaurantMenu
-            className="mb-2 text-theme-color-1 group-hover:text-white"
-            sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
-          />
-          <p
-            className="text-sm sm:text-2xl text-black group-hover:text-white cursor-pointer text-center"
+      <section className="my-20">
+        <div className="flex flex-row justify-between gap-2 lg:gap-10 lg:max-w-[1500px] lg:mx-auto">
+          <div
+            className="w-[calc(25%-0.5rem)] lg:w-[200px] lg:h-[200px] p-2 lg:p-5 flex flex-col gap-1 lg:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
             onClick={() => navigation()}
           >
-            Food Menu
-          </p>
+            <RestaurantMenu
+              className="mb-2 text-theme-color-1 group-hover:text-white"
+              sx={{ fontSize: isSmallScreen ? "40px" : "100px" }}
+            />
+            <p
+              className="text-[10px] sm:text-lg text-black group-hover:text-white cursor-pointer text-center"
+              onClick={() => navigation()}
+            >
+              Food Menu
+            </p>
+          </div>
+          <div
+            className="w-[calc(25%-0.5rem)] h-fit lg:w-[200px] lg:h-[200px] p-2 lg:p-5 flex flex-col gap-2 lg:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
+            onClick={() => navigation()}
+          >
+            <CalendarMonth
+              className="mb-2 text-theme-color-1 group-hover:text-white"
+              sx={{ fontSize: isSmallScreen ? "40px" : "100px" }}
+            />
+            <p
+              className="text-[10px] sm:text-lg text-black group-hover:text-white cursor-pointer text-center"
+              onClick={() => navigation()}
+            >
+              Meal Calendar
+            </p>
+          </div>
+          <div
+            className="w-[calc(25%-0.5rem)] h-fit lg:w-[200px] lg:h-[200px] p-2 lg:p-5 flex flex-col gap-2 lg:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
+            onClick={() => navigation()}
+          >
+            <PublishedWithChanges
+              className="mb-2 text-theme-color-1 group-hover:text-white"
+              sx={{ fontSize: isSmallScreen ? "40px" : "100px" }}
+            />
+            <p
+              className="text-[10px] sm:text-lg text-black group-hover:text-white cursor-pointer text-center"
+              onClick={() => navigation()}
+            >
+              Change Request
+            </p>
+          </div>
+          <div
+            className="w-[calc(25%-0.5rem)] h-fit lg:w-[200px] lg:h-[200px] p-2 lg:p-5 flex flex-col gap-2 lg:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
+            onClick={() => navigation()}
+          >
+            <DashboardCustomize
+              className="mb-2 text-theme-color-1 group-hover:text-white"
+              sx={{ fontSize: isSmallScreen ? "40px" : "100px" }}
+            />
+            <p
+              className="text-[10px] sm:text-lg text-black group-hover:text-white cursor-pointer text-center"
+              onClick={() => navigation()}
+            >
+              Customized Meal
+            </p>
+          </div>
         </div>
-        <div
-          className=" flex flex-col w-full h-[150px] sm:w-[200px] sm:h-[200px] p-2 sm:p-5 gap-2 sm:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
-          onClick={() => navigation()}
-        >
-          <CalendarMonth
-            className="mb-2 text-theme-color-1 group-hover:text-white"
-            sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
-          />
-          <p
-            className="text-sm sm:text-2xl text-black group-hover:text-white cursor-pointer text-center"
+        <div className="flex flex-row justify-between gap-2 lg:gap-10 lg:max-w-[1500px] lg:mx-auto mt-10">
+          <div
+            className="w-[calc(25%-0.5rem)] lg:w-[200px] lg:h-[200px] p-2 lg:p-5 flex flex-col gap-2 lg:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
             onClick={() => navigation()}
           >
-            Meal Calendar
-          </p>
-        </div>
-        <div
-          className=" flex flex-col w-full h-[150px] sm:w-[200px] sm:h-[200px] p-2 sm:p-5 gap-2 sm:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
-          onClick={() => navigation()}
-        >
-          <PublishedWithChanges
-            className="mb-2 text-theme-color-1 group-hover:text-white"
-            sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
-          />
-          <p
-            className="text-sm sm:text-2xl text-black group-hover:text-white cursor-pointer text-center"
+            <Receipt
+              className="mb-2 text-theme-color-1 group-hover:text-white"
+              sx={{ fontSize: isSmallScreen ? "40px" : "100px" }}
+            />
+            <p
+              className="text-[10px] sm:text-lg text-black group-hover:text-white cursor-pointer text-center"
+              onClick={() => navigation()}
+            >
+              Receipt
+            </p>
+          </div>
+          <div
+            className="w-[calc(25%-0.5rem)] h-fit lg:w-[200px] lg:h-[200px] p-2 lg:p-5 flex flex-col gap-2 lg:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
             onClick={() => navigation()}
           >
-            Change Request
-          </p>
-        </div>
-        <div
-          className="flex flex-col w-full h-[150px] sm:w-[200px] sm:h-[200px] p-2 sm:p-5 gap-2 sm:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
-          onClick={() => navigation()}
-        >
-          <DashboardCustomize
-            className="mb-2 text-theme-color-1 group-hover:text-white"
-            sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
-          />
-          <p
-            className="text-sm sm:text-2xl text-black group-hover:text-white cursor-pointer text-center"
+            <NotificationsActive
+              className="mb-2 text-theme-color-1 group-hover:text-white"
+              sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
+            />
+            <p
+              className="text-[10px] sm:text-lg text-black group-hover:text-white cursor-pointer text-center"
+              onClick={() => navigation()}
+            >
+              Subscription Plans
+            </p>
+          </div>
+          <div
+            className="w-[calc(25%-0.5rem)] lg:w-[200px] lg:h-[200px] p-2 lg:p-5 flex flex-col gap-2 lg:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
             onClick={() => navigation()}
           >
-            Custoomized Meal
-          </p>
-        </div>
-        <div
-          className="flex flex-col w-full h-[150px] sm:w-[200px] sm:h-[200px] p-2 sm:p-5 gap-2 sm:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
-          onClick={() => navigation()}
-        >
-          <Receipt
-            className="mb-2 text-theme-color-1 group-hover:text-white"
-            sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
-          />
-          <p
-            className="text-sm sm:text-2xl text-black group-hover:text-white cursor-pointer text-center"
+            <Help
+              className="mb-2 text-theme-color-1 group-hover:text-white"
+              sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
+            />
+            <p
+              className="text-[10px] sm:text-lg text-black group-hover:text-white cursor-pointer text-center"
+              onClick={() => navigation()}
+            >
+              Help
+            </p>
+          </div>
+          <div
+            className="w-[calc(25%-0.5rem)] h-fit lg:w-[200px] lg:h-[200px] p-2 lg:p-5 flex flex-col gap-2 lg:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
             onClick={() => navigation()}
           >
-            Receipt
-          </p>
-        </div>
-        <div
-          className="flex flex-col w-full h-[150px] sm:w-[200px] sm:h-[200px] p-2 sm:p-5 gap-2 sm:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
-          onClick={() => navigation()}
-        >
-          <NotificationsActive
-            className="mb-2 text-theme-color-1 group-hover:text-white"
-            sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
-          />
-          <p
-            className="text-sm sm:text-2xl text-black group-hover:text-white cursor-pointer text-center"
-            onClick={() => navigation()}
-          >
-            Subscription Plans
-          </p>
-        </div>
-        <div
-          className="flex flex-col w-full h-[150px] sm:w-[200px] sm:h-[200px] p-2 sm:p-5 gap-2 sm:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
-          onClick={() => navigation()}
-        >
-          <Help
-            className="mb-2 text-theme-color-1 group-hover:text-white"
-            sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
-          />
-          <p
-            className="text-sm sm:text-2xl text-black group-hover:text-white cursor-pointer text-center"
-            onClick={() => navigation()}
-          >
-            Help
-          </p>
-        </div>
-        <div
-          className="flex flex-col w-full h-[150px] sm:w-[200px] sm:h-[200px] p-2 sm:p-5 gap-2 sm:gap-5 justify-center items-center bg-white rounded group hover:bg-theme-color-1 shadow-md cursor-pointer"
-          onClick={() => navigation()}
-        >
-          <WhatsApp
-            className="mb-2 text-theme-color-1 group-hover:text-white"
-            sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
-          />
-          <p
-            className="text-sm sm:text-2xl text-black group-hover:text-white cursor-pointer text-center"
-            onClick={() => navigation()}
-          >
-            Chat on WhatsApp
-          </p>
+            <WhatsApp
+              className="mb-2 text-theme-color-1 group-hover:text-white"
+              sx={{ fontSize: isSmallScreen ? "60px" : "100px" }}
+            />
+            <p
+              className="text-[10px] sm:text-lg text-black group-hover:text-white cursor-pointer text-center"
+              onClick={() => navigation()}
+            >
+              Chat on WhatsApp
+            </p>
+          </div>
         </div>
       </section>
     </div>
