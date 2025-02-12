67c465c35c2b0f4347dbebc229a079e9d709aa23
mhmd-byi <mohammed@buildyourinnovation.com>
2025-02-12 16:32:24 +0530

    updating FAQs

---
+++ src/pages/Help/data.js
@@ -0,0 +1,42 @@
+export const faqData = [
+    {
+      question: "What Is Food Menu Feature?",
+      answer:
+        "Dear customer, with the help of food menu feature you can see previous & upcoming food menu of the month selecting date column.",
+    },
+    {
+      question: "What Is My Billings Feature?",
+      answer:
+        "Dear customer, with the help of My billings feature you can check all the details of the transaction you have made for the paln subscription.",
+    },
+    {
+      question: "What Is Meal Trackings Feature?",
+      answer:
+        "Dear customer, with the help of meal tracking feature you can track the dates you have made cancel request and how many meals have been delivered to you.",
+    },
+    {
+      question: "What Is Cancel Meal Request Feature?",
+      answer:
+        "Dear customer, with cancel meal request feature you can raise a request to cancel the meal for that day and future dates. please note you can raise cancel request from 12 Mid Night To Morning 11 for lunch & 12 Mid Night Till 4:30 PM for dinner.",
+    },
+    {
+      question: "What Is Customize Meal Feature?",
+      answer:
+        "Dear customer, customize meal feature , when you want to get something changed in you dish from today menu but please note you can raise cancel request from 12 Mid Night To Morning 11 for lunch & 12 Mid Night Till 4:30 PM for dinner.",
+    },
+    {
+      question: "What Is Subscription Plans Feature?",
+      answer:
+        "Dear Customer, subscription plans will show which plan you have opted currently and you can simply choose or upgrade to another plan.",
+    },
+    {
+      question: "What Is Help Feature?",
+      answer:
+        "Dear customer, Help feature is a form where you can raise a query around any subject related to webapp we will assist you within 24 hours.",
+    },
+    {
+      question: "What Is Chat On WhatsApp Feature?",
+      answer:
+        "Dear customer, chat on Whatsapp is feature which enables you to chat directly to our customer care number, please note wait time may be higher due to support team might be busy with other customers.",
+    },
+  ];
--- src/pages/Help/index.jsx
+++ src/pages/Help/index.jsx
@@ -1,34 +1,13 @@
 import React, { useState } from "react";
 import DashboardLayoutComponent from "../../components/common/Dashboard/Dashboard";
 import { ExpandMore, ExpandLess } from "@mui/icons-material";
+import { faqData } from "./data";
 
 export const HelpPage = () => {
   const [openItem, setOpenItem] = useState(0);
   const toggleAccordion = (index) => {
     setOpenItem(openItem === index ? null : index);
   };
-  const faqData = [
-    {
-      question: "How to create an account?",
-      answer:
-        "To create an account, find the 'Sign up' or 'Create account' button, fill out the registration form with your personal information, and click 'Create account' or 'Sign up.' Verify your email address if needed, and then log in to start using the platform.",
-    },
-    {
-      question: "Have any trust issue?",
-      answer:
-        "Our focus on providing robust and user-friendly content management capabilities ensures that you can manage your content with confidence, and achieve your content marketing goals with ease.",
-    },
-    {
-      question: "How can I reset my password?",
-      answer:
-        "To reset your password, click on the 'Forgot Password' link on the login page. Enter your email address, and we'll send you instructions to reset your password. Follow the link in the email to create a new password.",
-    },
-    {
-      question: "What is the payment process?",
-      answer:
-        "Our payment process is secure and straightforward. You can choose from various payment methods including credit/debit cards and PayPal. All transactions are encrypted to ensure your financial information remains safe.",
-    },
-  ];
 
   return (
     <DashboardLayoutComponent>
