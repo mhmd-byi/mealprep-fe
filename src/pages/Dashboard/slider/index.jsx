696db44cfbb216f9501cd9c16a9e7b70afa8336c
mhmd-byi <mohammed@buildyourinnovation.com>
2025-02-14 19:10:33 +0530

    replacing banners

--- src/pages/Dashboard/slider/index.jsx
+++ src/pages/Dashboard/slider/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
-import mealimage from "../../../assets/images/dashboard/meal.jpg";
-import mealimage2 from "../../../assets/images/dashboard/image2.jpg";
-import mealimage3 from "../../../assets/images/dashboard/image3.jpg";
+import slide1 from "../../../assets/images/dashboard/slide1.jpg";
+import slide2 from "../../../assets/images/dashboard/slide2.jpg";
+import slide3 from "../../../assets/images/dashboard/slide3.jpg";
 import { ArrowCircleLeft, ArrowCircleRight } from "@mui/icons-material";
 
 export const HeroSlider = () => {
@@ -11,7 +11,7 @@ export const HeroSlider = () => {
   const slides = [
     {
       id: 1,
-      image: mealimage,
+      image: slide1,
       // heading: "Meal Prep",
       // description:
       //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
@@ -19,7 +19,7 @@ export const HeroSlider = () => {
     },
     {
       id: 2,
-      image: mealimage2,
+      image: slide2,
       // heading: "Meal Prep",
       // description:
       //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -27,7 +27,7 @@ export const HeroSlider = () => {
     },
     {
       id: 3,
-      image: mealimage3,
+      image: slide3,
       // heading: "Meal Prep",
       // description:
       //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -65,47 +65,37 @@ export const HeroSlider = () => {
         {slides.map((slide, index) => (
           <div
             key={slide.id}
-            className={`relative w-full sm:w-[1500px] h-[350px] lg:h-[500px] mt-10 ${
+            className={`relative w-full sm:w-[1500px] h-auto mt-10 ${
               index !== currentSlide ? "hidden" : ""
             }`}
           >
-            <div className="hidden sm:block">
-              <img
-                src={slide.image}
-                className="w-full max-h-[250px] md:max-h-[450px] rounded-lg object-cover"
-              />
-              <div className=" p-8  absolute right-0 top-0 w-full sm:w-[50%] h-[450px] bg-black bg-opacity-75 flex flex-col justify-center px-4 sm:px-8 text-left rounded-lg">
-                <h1 className="text-white text-2xl sm:text-4xl font-bold mb-2 sm:mb-4">
-                  {slide.heading}
-                </h1>
-                <p className="text-white text-sm sm:text-lg mb-4 sm:mb-8">
-                  {slide.description}
-                </p>
-                <button className="bg-white text-black py-2 px-4 rounded w-fit">
-                  {slide.buttonText}
-                </button>
-              </div>
-            </div>
-            <div className="sm:hidden bg-theme-color-1 rounded-lg overflow-hidden shadow-lg">
-              <div className="w-full h-[250px]">
+            {/* Desktop and Mobile Layout (Combined) */}
+            <div className="bg-theme-color-1 rounded-lg overflow-hidden shadow-lg">
+              <div className="w-full">
                 <img
                   src={slide.image}
                   alt={slide.heading}
-                  className="w-full h-full object-cover"
+                  className="w-full h-[140px] md:h-[450px] lg:h-[500px] cover"
                 />
               </div>
 
-              <div className="p-4 text-white">
-                <h1 className="text-xl font-bold mb-2">{slide.heading}</h1>
-                <p className="text-sm mb-4">{slide.description}</p>
-                <button className="bg-white/30 text-white py-2 px-4 rounded hover:bg-opacity-90 transition-colors">
+              <div className="p-4 md:p-6 lg:p-8 text-white">
+                <h1 className="text-xl md:text-2xl lg:text-4xl font-bold mb-2 md:mb-4">
+                  {slide.heading}
+                </h1>
+                <p className="text-sm md:text-base lg:text-lg mb-4 md:mb-6">
+                  {slide.description}
+                </p>
+                <button className="bg-white text-black py-2 px-4 rounded hover:bg-opacity-90 transition-colors">
                   {slide.buttonText}
                 </button>
               </div>
             </div>
           </div>
         ))}
-        <div className="absolute -bottom-5 lg:bottom-12 left-1/2 -translate-x-1/2 flex space-x-2 bg-white p-1 rounded-full">
+
+        {/* Navigation Dots */}
+        <div className="absolute -bottom-5 lg:bottom-4 left-1/2 -translate-x-1/2 flex space-x-2 bg-white p-1 rounded-full">
           {slides.map((_, index) => (
             <div
               key={index}
@@ -118,27 +108,29 @@ export const HeroSlider = () => {
             ></div>
           ))}
         </div>
-        <div className="absolute hidden lg:block top-1/2 lg:left-2 sm:left-8 -translate-y-1/2 text-white cursor-pointer">
+
+        {/* Navigation Arrows */}
+        <div className="absolute hidden lg:block top-1/2 left-4 -translate-y-1/2 text-white cursor-pointer">
           <span
             className="material-icons"
             onClick={prevSlide}
-            style={{ fontSize: "30px", lineHeight: "30px" }}
+            style={{ fontSize: "40px", lineHeight: "40px" }}
           >
             <ArrowCircleLeft
-              sx={{ fontSize: "30px" }}
-              className="text-theme-color-1 hover:text-white"
+              sx={{ fontSize: "40px" }}
+              className="text-white/70 hover:text-white"
             />
           </span>
         </div>
-        <div className="absolute hidden lg:block top-1/2 right-2 sm:right-8 -translate-y-1/2 text-white cursor-pointer">
+        <div className="absolute hidden lg:block top-1/2 right-4 -translate-y-1/2 text-white cursor-pointer">
           <span
             className="material-icons"
             onClick={nextSlide}
-            style={{ fontSize: "30px", lineHeight: "30px" }}
+            style={{ fontSize: "40px", lineHeight: "40px" }}
           >
             <ArrowCircleRight
-              sx={{ fontSize: "30px" }}
-              className="text-theme-color-1 hover:text-white"
+              sx={{ fontSize: "40px" }}
+              className="text-white/70 hover:text-white"
             />
           </span>
         </div>
