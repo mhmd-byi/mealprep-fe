0e3b129482602fd4baa620b7a104683effa32506
mhmd-byi <mohammed@buildyourinnovation.com>
2025-02-15 16:33:46 +0530

    making customize meal page responsive

--- src/pages/customize-your-meal/index.jsx
+++ src/pages/customize-your-meal/index.jsx
@@ -34,16 +34,16 @@ export const CustomizeYourMeal = () => {
       <Helmet>
         <title>Customise Meal Request | Mealprep</title>
       </Helmet>
-      <div className="block lg:flex flex-col justify-center items-center p-5 w-full h-full">
-        <div className="py-12 px-4 sm:px-6 lg:px-8">
+      <div className="block lg:flex flex-col justify-center items-center p-2 sm:p-5 w-full h-full">
+        <div className="py-6 sm:py-12 px-2 sm:px-6 lg:px-8 w-full">
           <div className="mx-auto">
-            <div className="bg-white shadow-xl rounded-lg overflow-hidden max-w-7xl w-[850px]">
-              <div className="p-6 sm:p-10">
-                <h2 className="text-xl sm:text-2xl font-semibold mb-4 sm:mb-6 text-gray-800">
+            <div className="bg-white shadow-xl rounded-lg overflow-hidden w-full">
+              <div className="p-3 sm:p-10">
+                <h2 className="text-lg sm:text-xl lg:text-2xl font-semibold mb-3 sm:mb-6 text-gray-800">
                   Customise Meal Request
                 </h2>
-                <p className="mb-4">Steps to use:
-                  <ul>
+                <p className="mb-4 text-sm sm:text-base">Steps to use:
+                  <ul className="text-sm sm:text-base">
                     <li>1. First select the date and meal type for which you need to make customization request</li>
                     <li>2. Input your requests</li>
                     <li>3. Submit the request</li>
@@ -63,16 +63,16 @@ export const CustomizeYourMeal = () => {
                 {items.length > 1 ? (
                   <form onSubmit={handleSubmitCustomiseRequest}>
                     <div className="space-y-4">
-                      <h3 className="text-lg font-medium text-gray-900">
+                      <h3 className="text-base sm:text-lg font-medium text-gray-900">
                         Meal Items
                       </h3>
-                      <div className="max-h-[50vh] sm:max-h-[60vh] overflow-y-auto pr-2 space-y-4">
+                      <div className="max-h-[50vh] sm:max-h-[60vh] overflow-y-auto space-y-4">
                         {items.map((item, index) => (
                           <div
                             key={index}
-                            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 items-center p-4 bg-gray-100 rounded-lg"
+                            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 sm:gap-4 items-center p-2 sm:p-4 bg-gray-100 rounded-lg"
                           >
-                            <div>
+                            <div className="w-full">
                               <label className="block text-sm font-medium text-gray-700 mb-1">
                                 Item Name
                               </label>
@@ -84,7 +84,7 @@ export const CustomizeYourMeal = () => {
                                 className="w-full bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2"
                               />
                             </div>
-                            <div>
+                            <div className="w-full">
                               <label className="block text-sm font-medium text-gray-700 mb-1">
                                 Description
                               </label>
@@ -96,7 +96,7 @@ export const CustomizeYourMeal = () => {
                                 className="w-full bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2"
                               />
                             </div>
-                            <div>
+                            <div className="w-full">
                               <label className="block text-sm font-medium text-gray-700 mb-1">
                                 Weight
                               </label>
@@ -117,10 +117,10 @@ export const CustomizeYourMeal = () => {
                           </div>
                         ))}
                       </div>
-                      <div className="flex justify-between items-center">
+                      <div className="flex flex-col sm:flex-row justify-between items-center gap-3 sm:gap-0">
                         <Button
                           type="submit"
-                          classes="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+                          classes="w-full sm:w-auto bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
                         >
                           Submit
                         </Button>
@@ -134,9 +134,8 @@ export const CustomizeYourMeal = () => {
                             }]);
                             setStartDate("");
                             setMealType("");
-                          }
-                        }
-                          className="text-red-600 border-2 bg-white border-red-600 hover:bg-red-600 font-medium py-2 px-4 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50 hover:text-white"
+                          }}
+                          className="w-full sm:w-auto text-red-600 border-2 bg-white border-red-600 hover:bg-red-600 font-medium py-2 px-4 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50 hover:text-white"
                         >
                           Cancel
                         </button>
@@ -144,10 +143,10 @@ export const CustomizeYourMeal = () => {
                     </div>
                   </form>
                 ) : (
-                  <form onSubmit={handleSubmit}>
+                  <form onSubmit={handleSubmit} className="w-full">
                     <div className="flex flex-col space-y-4">
-                      <div className="flex flex-row space-x-4">
-                        <div className="w-1/2">
+                      <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+                        <div className="w-full sm:w-1/2">
                           <label
                             htmlFor="startDate"
                             className="block text-sm font-medium text-gray-700 mb-1"
@@ -164,7 +163,7 @@ export const CustomizeYourMeal = () => {
                             required
                           />
                         </div>
-                        <div className="w-1/2">
+                        <div className="w-full sm:w-1/2">
                           <label
                             htmlFor="mealType"
                             className="block text-sm font-medium text-gray-700 mb-1"
@@ -185,7 +184,7 @@ export const CustomizeYourMeal = () => {
                         </div>
                       </div>
                       <div>
-                        <Button type="submit" classes="w-1/4 text-white">
+                        <Button type="submit" classes="w-full sm:w-1/4 text-white">
                           Submit Request
                         </Button>
                       </div>
