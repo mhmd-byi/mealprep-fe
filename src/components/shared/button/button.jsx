f82a283a5bf274f91f005928d97201c924f2ef9e
huzefamehidpurwala <jamaliongithub@gmail.com>
2025-04-26 18:24:38 +0530

    add disabled functionality and make according to js coding standard

--- src/components/shared/button/button.jsx
+++ src/components/shared/button/button.jsx
@@ -1,14 +1,16 @@
 export const Button = ({
   type = "submit",
-  id,
-  children,
+  id = undefined,
+  children = undefined,
   classes = "",
-  onClick
+  onClick = undefined,
+  disabled = undefined
 }) => {
   const defaultClasses = `flex justify-center rounded-md bg-theme-color-1 px-5 py-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`;
-  const combinedClasses = `${defaultClasses} ${classes}`;
+  const disabledBtnClasses = "!bg-[#295f3f] !cursor-not-allowed !pointer-events-none"
+  const combinedClasses = `${defaultClasses} ${classes} ${disabled === true ? disabledBtnClasses : ""}`;
   return (
-    <button id={id} type={type} className={combinedClasses} onClick={onClick}>
+    <button id={id} type={type} className={combinedClasses} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
