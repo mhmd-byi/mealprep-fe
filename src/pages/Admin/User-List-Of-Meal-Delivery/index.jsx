99262ea3cba7433e2be72e7320282d12a9d1f952
mhmd-byi <mohammed@buildyourinnovation.com>
2026-09-12 11:25:37 +0530

    fix: export pdf in meal delivery list

--- src/pages/Admin/User-List-Of-Meal-Delivery/index.jsx
+++ src/pages/Admin/User-List-Of-Meal-Delivery/index.jsx
@@ -351,12 +351,14 @@ export const UserListOfMealDelivery = () => {
                             Filter
                           </button>
                           <Button
+                            type="button"
                             onClick={() => exportToCSV(sortedMeals)}
                             className="px-4 py-2 font-medium text-white bg-blue-500 rounded-lg transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
                           >
                             Export to CSV
                           </Button>
                           <Button
+                            type="button"
                             onClick={() => setIsPrinting(true)}
                             className="px-4 py-2 font-semibold bg-white rounded-lg border-2 shadow-sm transition-colors duration-300 text-theme-color-1 border-theme-color-1 hover:bg-theme-color-1 hover:text-white"
                           >
